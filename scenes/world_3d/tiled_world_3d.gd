308dba587aa16c9034ec17802af972681a75da97
kitfox <mark@kitfox.com>
2025-12-20 00:22:32 -0500

    Drawing tilemap on plane.

--- scenes/world_3d/tiled_world_3d.gd
+++ scenes/world_3d/tiled_world_3d.gd
@@ -1,32 +1,17 @@
 extends Node3D
 class_name TiledWorld3D
 
+@onready var world_tilemap:WorldTilemap2D = %tilemaps
 
-var map:WorldMap
+@export var map:WorldMap
+@export var map_seed:int
 
 
 func build_layout_from_map(map:WorldMap):
-	var isoBuilder:IsoTileLayoutBuilder = IsoTileLayoutBuilder.new()
-	isoBuilder.map = map
-	isoBuilder.continent_layer = %continent
-	isoBuilder.desert_layer = %desert
-	isoBuilder.grass_layer = %grass
-	isoBuilder.feature_layer = %features
+	world_tilemap.map = map
+	world_tilemap.map_seed = map_seed
+	world_tilemap.rebuild_map()
 	
-	isoBuilder.rng = RandomNumberGenerator.new()
-	isoBuilder.rng.seed = %spin_seed.value
-	#isoBuilder.dump_tileset()
-	isoBuilder.build()
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_file_dialog_load_file_selected(path: String) -> void:
 	var loaded_map = ResourceLoader.load(path)
 	if loaded_map is WorldMap:
