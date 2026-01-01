2dbfb36b41d788f6eb347bd9446aca26f0e82267
kitfox <mark@kitfox.com>
2025-12-31 23:24:50 -0500

    Adding features to map.

--- scenes/world_3d/tiled_world_3d.gd
+++ scenes/world_3d/tiled_world_3d.gd
@@ -4,6 +4,8 @@ class_name TiledWorld3D
 
 @onready var world_tilemap:WorldTilemap2D = %tilemaps
 @onready var subviewport:SubViewport = %SubViewport
+@onready var ground_mesh:MeshInstance3D = %MeshInstance3D
+@onready var features:Node3D = %features
 
 @export var map:WorldMap:
 	set(v):
@@ -33,25 +35,59 @@ class_name TiledWorld3D
 		tile_size = v
 		update_from_map()
 
+@export var mountain_scene:PackedScene = preload("res://scenes/world_3d/props/mountain.tscn")
+@export var hill_scene:PackedScene = preload("res://scenes/world_3d/props/hill.tscn")
+@export var trees_scene:PackedScene = preload("res://scenes/world_3d/props/trees.tscn")
+
 func update_from_map():
 	if !is_node_ready():
 		return
-		
+	
+	for child in features.get_children():
+		child.queue_free()
+	
 	if !map:
 		return
 	
 	#Height divided by 2 since rows are staggered
-	subviewport.size = Vector2i(map.get_width() * tile_size.x, map.get_height() * tile_size.y / 2)
+	var map_pixel_size:Vector2i = Vector2(map.get_width() * tile_size.x, map.get_height() * tile_size.y / 2.0)
+	subviewport.size = map_pixel_size
 	
+	ground_mesh.scale = Vector3(map.get_width(), 1, map.get_height() / 2.0) / 2.0
 	
-	pass
+	#Create map features
+	var feature_offset:Vector3 = -Vector3(map.get_width(), 0, map.get_height() / 2.0) / 2.0
+	
+	for j in map.get_height():
+		for i in map.get_width():
+			var cell:WorldMapCell = map.get_cell_v(Vector2i(i, j))
+			
+			var feature:Node3D
+			
+			if cell.feature == WorldMapCell.Feature.MOUNTAIN:
+				feature = mountain_scene.instantiate()
+			elif cell.feature == WorldMapCell.Feature.HILL:
+				feature = hill_scene.instantiate()
+			elif cell.vegetation == WorldMapCell.Vegetation.FOREST:
+				feature = trees_scene.instantiate()
+			
+			if feature:
+				features.add_child(feature)
+				
+				var pos:Vector3 = Vector3(float(i), 0, float(j) / 2.0) + feature_offset
+				feature.position = pos
+				if (j & 0b1) == 1:
+					#feature.position.y += .5
+					feature.position.x += .5
+			
 
 func _ready() -> void:
 	world_tilemap.map = map
 	world_tilemap.map_seed = map_seed
+	update_from_map()
 
-func build_layout_from_map(map:WorldMap):
-	world_tilemap.map = map
+func build_layout_from_map(new_map:WorldMap):
+	world_tilemap.map = new_map
 	world_tilemap.map_seed = map_seed
 	world_tilemap.rebuild_map()
 	
